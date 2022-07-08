5351bca935b2d2748c5aa043be6767d5e4f8e1a3
teumiloo <nguyentienanh20122000@gmail.com>
2022-07-08 15:35:52 +0700

    Chapter3_4_5

--- app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-module ApplicationHelper
-  def full_title page_title
-    base_title = "Ruby on Rails Tutorial Sample App"
-    page_title.empty? ? base_title : [page_title, base_title].join(" | ")
-  end
-end
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root 'static_pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
