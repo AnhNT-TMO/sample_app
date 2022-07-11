8f07d840689f3b3d65a3312b71c1007e9ba5ec32
teumiloo <nguyentienanh20122000@gmail.com>
2022-07-11 07:45:08 +0700

    Chapter 7

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  before_action :find_by_email, only: :create
+
+  def new; end
+
+  def create
+    if @user&.authenticate params[:session][:password]
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_to @root_path
+    else
+      flash.now[:danger] = t ".alert"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+
+    redirect_to login_url
+  end
+
+  private
+
+  def find_by_email
+    @user = User.find_by email: params.dig(:session, :email)&.downcase
+    return if @user
+
+    flash[:danger] = t ".controller.find_error"
+    redirect_to root_path
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by params[:id]
+    return if @user
+
+    flash[:danger] = t ".alert_user"
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".alert"
+      redirect_to root_path
+    else
+      flash.now[:danger] = t ".alert_not_save"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(User::UPDATABLE_ATTRS)
+  end
 end
