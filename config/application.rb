98e56ef388070533f5c1f13e0862392ee0f473f2
teumiloo <nguyentienanh20122000@gmail.com>
2022-07-11 07:45:08 +0700

    Chapter 3 4 5

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,12 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.load_path +=
+      Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     # Configuration for the application, engines, and railties goes here.
     #
+    config.i18n.available_locales = %i[en vi]
+    config.i18n.default_locale = :en
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
